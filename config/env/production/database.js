de995971e0435ce5afec896252f15e77f1d95e9c
Rémy Mondi <remy@mondi.dev>
2023-07-03 09:57:01 +0200

    [Delete] Platform config

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -6,7 +6,10 @@ module.exports = ({ env }) => ({
       port: env.int('DATABASE_PORT'),
       database: env('DATABASE_NAME'),
       user: env('DATABASE_USERNAME'),
-      password: env('DATABASE_PASSWORD')
+      password: env('DATABASE_PASSWORD'),
+      ssl: {
+        rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
+      },
     },
     debug: false,
   },
